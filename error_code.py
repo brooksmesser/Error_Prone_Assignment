dc882c2d42113e83deede7c7f751a7ee78da02c6
brooksmesser <bm536931@georgetownisd.org>
2026-02-05 15:24:58 -0600

    debuggs
    line 5 put "_" for create_monster line 10 deleted items line 90 appended name to creat_monster function line 107 made Luck luck

--- error_code.py
+++ error_code.py
@@ -2,12 +2,11 @@
 # Use what you know about Python to fix them
 
 import random
-from error_code_function import combat, createMonster  
+from error_code_functions import combat, create_monster  
 
 char_name = input("Please enter a character name:\n")
 char_class = 0
 items = ["sword","shield","potion","spyglass","wand","dagger","spellbook","coinpurse"]
-items = [] 
 class_stats = {
     "Fighter":{
         "health":100,
@@ -87,7 +86,7 @@ while True:
                 print("\nThe fight drags on... neither side can land a clean hit.")
                 print("You break away and retreat before this becomes your whole personality.")
         elif monster["health"] > 0:
-            print(f"You were slain by the {monster["name"]}!") 
+            print(f"You were slain by the {create_monster.name}!") 
             print("Your vision fades. The dungeon claims another hero.")
             break
         else:
@@ -104,8 +103,8 @@ while True:
     elif choice == "Search" or choice == "2":
         did_find = random.randint(1,100)
 
-        if did_find < char_class["Luck"]:
-            found_item = random.choice(items) 
+        if did_find < char_class["luck"]:
+            found_item = random.choice(items)
             char_class["inventory"].append(found_item)
             print(f"You search the area... and find a {found_item}!")
             print(f"Inventory: {char_class['inventory']}")
